9ef3ea26348486a4dda3677360dbcb592058abdc
Android-Instructor <andres.vasquez.a@hotmail.com>
2017-11-17 20:32:41 -0400

    First commit.

---
+++ app/src/main/java/com/example/android_instructor/navigationdrawer/CuatroFragment.java
@@ -0,0 +1,36 @@
+package com.example.android_instructor.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Android-Instructor on 17/11/2017.
+ */
+
+public class CuatroFragment extends Fragment{
+
+    private TextView texto;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cuatro, container, false);
+        texto = (TextView)view.findViewById(R.id.texto);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        texto.setTextSize(18);
+    }
+}
